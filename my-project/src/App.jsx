b2cdb2c8ca4f63233a6253d95d24147232e272e4
Yago Cima Castelao <yago_castelau@hotmail.com>
2024-12-04 03:08:02 +0100

    Background y Navbar

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
+import MainImg from "./assets/magic.png";
+
+const bgImage = {
+  backgroundImage: `url(${MainImg})`,
+  height: "100vh",
+  width: "100%",
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+};
 
 const App = () => {
   return (
   <>
-  <main>
+  <main style={bgImage}>
     <Navbar />
     <Hero />
   </main>
--- my-project/src/components/Navbar/Navbar.jsx
+++ my-project/src/components/Navbar/Navbar.jsx
@@ -3,7 +3,31 @@ import React from "react";
 const Navbar = () => {
   return (
   <>
-    <div>Navbar</div>
+    <nav className="bg-white shadow-lg">
+      <div className="container flex justify-between items-center ">
+        {/*logo section*/}
+        <a href="#" className="text-3xl font-bold text-gray-800">Logo</a>
+        {/*menu section*/}
+        <div className="hidden md:block">
+          <ul className="flex font-semibold justify-center items-center gap-4">
+            <li>
+              <a href="#"
+              className="text-gray-800 hover:text-gray-600 inline-block px-4 py-4 select-none"
+              >Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+            <li>
+              <a href="#">Login</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   </>
   ); 
 };
